437214a42e40d3e86f8bdf143754886bdc48cc9e
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2020-02-01 16:25:36 +0200

    moving to vue

--- asteroid.js
+++ asteroid.js
@@ -1,3 +1,11 @@
+var asteroid = new Vue({
+  el: '#asteroid',
+  data: {
+    asteroidList: [],
+    message: "Loading NASA asteroid data..."
+  }
+})
+
 function init() {
   // Might not give right day if timezones and stuff, but whatever
   const date = new Date().toISOString().slice(0,10);
@@ -5,7 +13,6 @@ function init() {
   const apikey = '0nvq8nAqwKq9EtUBqyS7lL5dHv3A3O2YKhIaS86V'
 
   url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${date}&end_date=${date}&api_key=${apikey}`
-  document.getElementById("deathMiss").textContent = 'Loading NASA data...'
   fetch(url)
   .then((response) => {
     return response.json();
@@ -16,8 +23,6 @@ function init() {
 
 }
 function displayData(data, date) {
-  const listElement = document.getElementById("asteroidList");
-  const pElement = document.getElementById("deathMiss");
   let asteroids = data.near_earth_objects[date];
   let astDist = [];
 
@@ -27,27 +32,11 @@ function displayData(data, date) {
 
   astDist.sort((n1,n2) => n1.distance - n2.distance);
 
-  for (a in astDist) {
-    // Create list of type
-    // [url](name) is x km away
-    let node = document.createElement("LI");
-    let span = document.createElement('span')
-    let link = document.createElement('a');
-    let linktext = document.createTextNode(`${astDist[a].name}`);
-    let textnode = document.createTextNode(` is only ${astDist[a].distance.toLocaleString()} km from colliding with Earth!`);
-    link.appendChild(linktext);
-    link.title = astDist[a].name;
-    link.href = astDist[a].link;
-    span.appendChild(link);
-    span.appendChild(textnode);
-    node.appendChild(span);
-    listElement.appendChild(node);
-  }
-
+  asteroid.asteroidList = astDist;
   if (astDist.length > 0) {
     // If Earth got hit by asteroid, nobody probably would read this anymore
-    pElement.textContent = `We missed destruction by mere ${astDist[0].distance.toLocaleString()} kilometers today!`
+    asteroid.message = `We missed destruction by mere ${astDist[0].distance.toLocaleString()} kilometers today!`
   } else {
-    pElement.textContent = 'No asteroids reached their closest-to-earth point today. Or something went wrong.'
+    asteroid.message = 'No asteroids reached their closest-to-earth point today. Or something went wrong.'
   }
 }
