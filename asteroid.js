436ea015cf8ab65bf5d41787de5ef9283df71dae
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2020-02-04 18:20:27 +0200

    Visualize distance to asteroid

--- asteroid.js
+++ asteroid.js
@@ -1,17 +1,56 @@
 const distanceToMoon = 384400; // average distance from Earth
-const distanceToSun = 149600000; // again, on average
 
 var asteroid = new Vue({
   el: '#asteroid',
   data: {
     asteroidList: [],
     message: "Loading NASA asteroid data...",
-    toMoon: distanceToMoon / distanceToSun,
-    toSun: 1,
-    toAsteroid: 0
+    toMoon: 0,
+    drawCanvas: null,
+    canvasWidth: 0
   },
   mounted: function() {
     loadData();
+    let c = document.getElementById("aCanvas");
+    this.canvasWidth = c.width;
+    let ctx = c.getContext("2d");
+    this.drawCanvas = ctx;
+  },
+  methods: {
+    canvasRepaint() {
+      // Asteroid should always be more far away, so put it at the very end of canvas
+      let aX = this.canvasWidth - 50;
+
+      let mX = aX * this.toMoon;
+
+      this.drawCanvas.font = "12px Georgia";
+
+      this.drawCanvas.fillStyle = 'gray';
+      this.drawCanvas.fillRect(0, 0, this.canvasWidth, 100);
+
+      this.drawCanvas.beginPath();
+      this.drawCanvas.arc(25, 25, 20, 0, 2 * Math.PI, false);
+      this.drawCanvas.fillStyle = 'green';
+      this.drawCanvas.fill();
+      this.drawCanvas.fillText("Earth", 10, 55);
+
+      this.drawCanvas.beginPath();
+      this.drawCanvas.arc(mX, 25, 20, 0, 2 * Math.PI, false);
+      this.drawCanvas.fillStyle = 'white';
+      this.drawCanvas.fill();
+      this.drawCanvas.fillText("Moon", mX-15, 55);
+
+      this.drawCanvas.beginPath();
+      this.drawCanvas.arc(aX, 25, 20, 0, 2 * Math.PI, false);
+      this.drawCanvas.fillStyle = 'black';
+      this.drawCanvas.fill();
+      this.drawCanvas.fillText("Asteroid", aX-20, 55);
+    }
+  },
+  watch: {
+    toMoon: function() {
+      this.canvasRepaint();
+    }
   }
 })
 
@@ -44,8 +83,8 @@ function displayData(data, date) {
   asteroid.asteroidList = astDist;
   if (astDist.length > 0) {
     // distances for visualization
-    asteroid.toAsteroid = astDist[0].distance / distanceToSun;
-
+    asteroid.toMoon = distanceToMoon / astDist[0].distance;
+    asteroid.canvasRepaint();
     // If Earth got hit by asteroid, nobody probably would read this anymore
     asteroid.message = `We missed destruction by mere ${astDist[0].distance.toLocaleString()} kilometers today!`
   } else {
